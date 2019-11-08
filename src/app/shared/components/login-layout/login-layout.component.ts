ed7e8820d40db305f6cf33e21e21124c1a6b827f
Maxmiliano Reipert Andriani <max.andriani@becomex.com.br>
2019-11-08 13:54:54 -0300

    Correção de CSS do LoginLayout

--- src/app/shared/components/login-layout/login-layout.component.ts
+++ src/app/shared/components/login-layout/login-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'tda-login-layout',
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-layout.component.scss']
 })
 export class LoginLayoutComponent implements OnInit {
+
+  // tslint:disable-next-line:variable-name
+  protected _class: string[];
+  @Input()
+  @HostBinding('class')
+  set class(css: string) {
+    this._class = css.split(' ');
+  }
+  get class(): string {
+    return ['login-layout', ...this._class].join(' ');
+  }
 
   constructor() { }
 
